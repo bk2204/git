7a3877b623192afd36d47eb7105e546d37938d10
Jens Lehmann <Jens.Lehmann@web.de>
2013-04-05 18:35:27 +0200

    submodule: teach unpack_trees() to update submodules
    Signed-off-by: Jens Lehmann <Jens.Lehmann@web.de>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- submodule.h
+++ submodule.h
@@ -51,13 +51,16 @@ void set_config_update_recurse_submodules(int default_value, int option_value);
 int submodule_needs_update(const char *path);
 int populate_submodule(const char *path, unsigned char sha1[20], int force);
 int depopulate_submodule(const char *path);
+int update_submodule(const char *path, const unsigned char sha1[20], int force);
 void check_for_new_submodule_commits(unsigned char new_sha1[20]);
 int fetch_populated_submodules(const struct argv_array *options,
 			       const char *prefix, int command_line_option,
 			       int quiet, int max_parallel_jobs);
+int is_submodule_populated(const char *path);
 unsigned is_submodule_modified(const char *path, int ignore_untracked);
 int submodule_uses_gitfile(const char *path);
 int ok_to_remove_submodule(const char *path);
+unsigned is_submodule_checkout_safe(const char *path, const unsigned char sha1[20]);
 int merge_submodule(unsigned char result[20], const char *path, const unsigned char base[20],
 		    const unsigned char a[20], const unsigned char b[20], int search);
 int find_unpushed_submodules(unsigned char new_sha1[20], const char *remotes_name,

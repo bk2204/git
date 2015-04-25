0f2f540a248fc92b527169fb2af3bec13262fff2
Michael Haggerty <mhagger@alum.mit.edu>
2015-04-25 12:12:01 +0200

    show_tag_lines(): rewrite to take an object_id argument
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>

--- builtin/tag.c
+++ builtin/tag.c
@@ -176,7 +176,7 @@ static enum contains_result contains(struct commit *candidate,
 	return contains_test(candidate, want);
 }
 
-static void show_tag_lines(const unsigned char *sha1, int lines)
+static void show_tag_lines(const struct object_id *oid, int lines)
 {
 	int i;
 	unsigned long size;
@@ -184,14 +184,14 @@ static void show_tag_lines(const unsigned char *sha1, int lines)
 	char *buf, *sp, *eol;
 	size_t len;
 
-	buf = read_sha1_file(sha1, &type, &size);
+	buf = read_sha1_file(oid->hash, &type, &size);
 	if (!buf)
-		die_errno("unable to read object %s", sha1_to_hex(sha1));
+		die_errno("unable to read object %s", oid_to_hex(oid));
 	if (type != OBJ_COMMIT && type != OBJ_TAG)
 		goto free_return;
 	if (!size)
 		die("an empty %s object %s?",
-		    typename(type), sha1_to_hex(sha1));
+		    typename(type), oid_to_hex(oid));
 
 	/* skip header */
 	sp = strstr(buf, "\n\n");
@@ -242,7 +242,7 @@ static int show_reference(const char *refname, const struct object_id *oid,
 			return 0;
 		}
 		printf("%-15s ", refname);
-		show_tag_lines(oid->hash, filter->lines);
+		show_tag_lines(oid, filter->lines);
 		putchar('\n');
 	}
 

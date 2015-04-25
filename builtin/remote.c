b0bafe2ff7ef9b8670fca4dd42babad170fb04de
Michael Haggerty <mhagger@alum.mit.edu>
2015-04-25 12:04:05 +0200

    mv(): convert local variable sha1 to object_id
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>

--- builtin/remote.c
+++ builtin/remote.c
@@ -701,9 +701,9 @@ static int mv(int argc, const char **argv)
 	for (i = 0; i < remote_branches.nr; i++) {
 		struct string_list_item *item = remote_branches.items + i;
 		int flag = 0;
-		unsigned char sha1[20];
+		struct object_id oid;
 
-		read_ref_full(item->string, RESOLVE_REF_READING, sha1, &flag);
+		read_ref_full(item->string, RESOLVE_REF_READING, oid.hash, &flag);
 		if (!(flag & REF_ISSYMREF))
 			continue;
 		if (delete_ref(item->string, NULL, REF_NODEREF))

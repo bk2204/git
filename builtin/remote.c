9ba48c371af85b57a8c6be00ec3b8b607f26af2c
Michael Haggerty <mhagger@alum.mit.edu>
2015-04-25 12:02:07 +0200

    read_remote_branches(): convert local variable orig_sha1 to object_id
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>

--- builtin/remote.c
+++ builtin/remote.c
@@ -560,14 +560,14 @@ static int read_remote_branches(const char *refname,
 	struct strbuf buf = STRBUF_INIT;
 	struct string_list_item *item;
 	int flag;
-	unsigned char orig_sha1[20];
+	struct object_id orig_oid;
 	const char *symref;
 
 	strbuf_addf(&buf, "refs/remotes/%s/", rename->old);
 	if (starts_with(refname, buf.buf)) {
 		item = string_list_append(rename->remote_branches, xstrdup(refname));
 		symref = resolve_ref_unsafe(refname, RESOLVE_REF_READING,
-					    orig_sha1, &flag);
+					    orig_oid.hash, &flag);
 		if (flag & REF_ISSYMREF)
 			item->util = xstrdup(symref);
 		else

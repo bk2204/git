2cd4cad70e9710dafa9c08096189377fad78fa70
Michael Haggerty <mhagger@alum.mit.edu>
2015-04-25 12:08:59 +0200

    cmd_show_ref(): convert local variable sha1 to object_id
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>

--- builtin/show-ref.c
+++ builtin/show-ref.c
@@ -210,12 +210,12 @@ int cmd_show_ref(int argc, const char **argv, const char *prefix)
 		if (!pattern)
 			die("--verify requires a reference");
 		while (*pattern) {
-			unsigned char sha1[20];
+			struct object_id oid;
 
 			if (starts_with(*pattern, "refs/") &&
-			    !read_ref(*pattern, sha1)) {
+			    !read_ref(*pattern, oid.hash)) {
 				if (!quiet)
-					show_one(*pattern, sha1);
+					show_one(*pattern, oid.hash);
 			}
 			else if (!quiet)
 				die("'%s' - not a valid ref", *pattern);

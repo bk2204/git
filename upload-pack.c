5e2f48ceca32a7e5f53637aa77eac20cff067e2d
Michael Haggerty <mhagger@alum.mit.edu>
2015-04-25 12:41:08 +0200

    mark_our_ref(): rewrite to take an object_id argument
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>

--- upload-pack.c
+++ upload-pack.c
@@ -681,9 +681,9 @@ static void receive_needs(void)
 }
 
 /* return non-zero if the ref is hidden, otherwise 0 */
-static int mark_our_ref(const char *refname, const unsigned char *sha1)
+static int mark_our_ref(const char *refname, const struct object_id *oid)
 {
-	struct object *o = lookup_unknown_object(sha1);
+	struct object *o = lookup_unknown_object(oid->hash);
 
 	if (ref_is_hidden(refname)) {
 		o->flags |= HIDDEN_REF;
@@ -696,7 +696,7 @@ static int mark_our_ref(const char *refname, const unsigned char *sha1)
 static int check_ref(const char *refname, const struct object_id *oid,
 		     int flag, void *cb_data)
 {
-	mark_our_ref(refname, oid->hash);
+	mark_our_ref(refname, oid);
 	return 0;
 }
 
@@ -719,7 +719,7 @@ static int send_ref(const char *refname, const struct object_id *oid,
 	const char *refname_nons = strip_namespace(refname);
 	struct object_id peeled;
 
-	if (mark_our_ref(refname, oid->hash))
+	if (mark_our_ref(refname, oid))
 		return 0;
 
 	if (capabilities) {

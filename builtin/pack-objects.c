ca5ab979a198ee60883c18e78830b460c36dea2a
Michael Haggerty <mhagger@alum.mit.edu>
2015-04-26 07:38:08 +0200

    add_ref_tag(): convert local variable "peeled" to object_id
    Signed-off-by: Michael Haggerty <mhagger@alum.mit.edu>

--- builtin/pack-objects.c
+++ builtin/pack-objects.c
@@ -2099,11 +2099,11 @@ static void ll_find_deltas(struct object_entry **list, unsigned list_size,
 
 static int add_ref_tag(const char *path, const struct object_id *oid, int flag, void *cb_data)
 {
-	unsigned char peeled[20];
+	struct object_id peeled;
 
 	if (starts_with(path, "refs/tags/") && /* is a tag? */
-	    !peel_ref(path, peeled)        && /* peelable? */
-	    packlist_find(&to_pack, peeled, NULL))      /* object packed? */
+	    !peel_ref(path, peeled.hash)    && /* peelable? */
+	    packlist_find(&to_pack, peeled.hash, NULL))      /* object packed? */
 		add_object_entry(oid->hash, OBJ_TAG, NULL, 0);
 	return 0;
 }

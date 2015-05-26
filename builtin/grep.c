4ae9bf1d615cac09d45a3f248cc586284a6758cf
brian m. carlson <sandals@crustytoothpaste.net>
2015-05-26 01:00:13 +0000

    cmd_grep: convert to struct object_id
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- builtin/grep.c
+++ builtin/grep.c
@@ -814,11 +814,11 @@ int cmd_grep(int argc, const char **argv, const char *prefix)
 	/* Check revs and then paths */
 	for (i = 0; i < argc; i++) {
 		const char *arg = argv[i];
-		unsigned char sha1[20];
+		struct object_id oid;
 		struct object_context oc;
 		/* Is it a rev? */
-		if (!get_sha1_with_context(arg, 0, sha1, &oc)) {
-			struct object *object = parse_object_or_die(sha1, arg);
+		if (!get_sha1_with_context(arg, 0, oid.hash, &oc)) {
+			struct object *object = parse_object_or_die(oid.hash, arg);
 			if (!seen_dashdash)
 				verify_non_filename(prefix, arg);
 			add_object_array_with_path(object, arg, &list, oc.mode, oc.path);

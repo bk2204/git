3594ae164971ee3b21375d876de50a3cef18e898
brian m. carlson <sandals@crustytoothpaste.net>
2017-05-21 18:58:52 +0000

    commit: convert commit_tree* to struct object_id
    Convert commit_tree and commit_tree extended to take pointers to struct
    object_id.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- builtin/am.c
+++ builtin/am.c
@@ -1641,7 +1641,7 @@ static void do_commit(const struct am_state *state)
 		setenv("GIT_COMMITTER_DATE",
 			state->ignore_date ? "" : state->author_date, 1);
 
-	if (commit_tree(state->msg, state->msg_len, tree.hash, parents, commit.hash,
+	if (commit_tree(state->msg, state->msg_len, &tree, parents, &commit,
 				author, state->sign_commit))
 		die(_("failed to write commit object"));
 

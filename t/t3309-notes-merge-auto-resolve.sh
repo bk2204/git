8b808c2c83e6c04ea07d46542cfe0e32cb315750
brian m. carlson <sandals@crustytoothpaste.net>
2018-04-07 23:22:32 +0000

    t: skip notes merge tests if not using SHA-1
    There are several notes merge tests which make extensive use of object
    IDs for the notes.  These values are hard to compute using the standard
    plumbing commands, so skip these tests for now if we're not using SHA-1.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- t/t3309-notes-merge-auto-resolve.sh
+++ t/t3309-notes-merge-auto-resolve.sh
@@ -7,6 +7,12 @@ test_description='Test notes merging with auto-resolving strategies'
 
 . ./test-lib.sh
 
+if ! test_have_prereq SHA1
+then
+       skip_all='not using SHA-1 for objects'
+       test_done
+fi
+
 # Set up a notes merge scenario with all kinds of potential conflicts
 test_expect_success 'setup commits' '
 	test_commit 1st &&

10b7d72725bd750251a6f9d936ce489dd23e5428
brian m. carlson <sandals@crustytoothpaste.net>
2018-05-23 01:07:44 +0000

    parse-options-cb: convert to use the_hash_algo
    Replace a hard-coded constant with uses of the_hash_algo.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- parse-options-cb.c
+++ parse-options-cb.c
@@ -21,8 +21,8 @@ int parse_opt_abbrev_cb(const struct option *opt, const char *arg, int unset)
 			return opterror(opt, "expects a numerical value", 0);
 		if (v && v < MINIMUM_ABBREV)
 			v = MINIMUM_ABBREV;
-		else if (v > 40)
-			v = 40;
+		else if (v > the_hash_algo->hexsz)
+			v = the_hash_algo->hexsz;
 	}
 	*(int *)(opt->value) = v;
 	return 0;

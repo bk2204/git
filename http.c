1b85b470c473800483e4f48b1f16290bbb6615bd
brian m. carlson <sandals@crustytoothpaste.net>
2018-05-26 01:39:41 +0000

    http: convert struct http_object_request to object_id
    Change two struct members of this function from unsigned char arrays to
    struct object_id.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- http.c
+++ http.c
@@ -2245,7 +2245,7 @@ struct http_object_request *new_http_object_request(const char *base_url,
 
 	freq = xcalloc(1, sizeof(*freq));
 	strbuf_init(&freq->tmpfile, 0);
-	hashcpy(freq->sha1, sha1);
+	hashcpy(freq->oid.hash, sha1);
 	freq->localfile = -1;
 
 	sha1_file_name(the_repository, &filename, sha1);
@@ -2390,16 +2390,16 @@ int finish_http_object_request(struct http_object_request *freq)
 	}
 
 	git_inflate_end(&freq->stream);
-	git_SHA1_Final(freq->real_sha1, &freq->c);
+	git_SHA1_Final(freq->real_oid.hash, &freq->c);
 	if (freq->zret != Z_STREAM_END) {
 		unlink_or_warn(freq->tmpfile.buf);
 		return -1;
 	}
-	if (hashcmp(freq->sha1, freq->real_sha1)) {
+	if (oidcmp(&freq->oid, &freq->real_oid)) {
 		unlink_or_warn(freq->tmpfile.buf);
 		return -1;
 	}
-	sha1_file_name(the_repository, &filename, freq->sha1);
+	sha1_file_name(the_repository, &filename, freq->oid.hash);
 	freq->rename = finalize_object_file(freq->tmpfile.buf, filename.buf);
 	strbuf_release(&filename);
 

ed303a8f7dfc02dcd5b0bb768df9ebc8dba15169
brian m. carlson <sandals@crustytoothpaste.net>
2021-01-25 01:59:23 +0000

    bulk-checkin: hash object with compatibility algorithm
    Any time we write an object into the repository when we're in dual hash
    mode, we need to compute both algorithms.  We already do this when we
    write a loose object into the repository, but we also need to do so in
    the other case we write an object, which is the bulk check-in code.
    
    For now, since we don't have any way to write the required v3 pack
    indices, we write the object into the loose object index.  We'll revert
    that change in the future when we write it directly into the index.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- bulk-checkin.c
+++ bulk-checkin.c
@@ -9,6 +9,7 @@
 #include "strbuf.h"
 #include "packfile.h"
 #include "object-store.h"
+#include "loose.h"
 
 static struct bulk_checkin_state {
 	unsigned plugged:1;
@@ -95,7 +96,8 @@ static int already_written(struct bulk_checkin_state *state, struct object_id *o
  * with a new pack.
  */
 static int stream_to_pack(struct bulk_checkin_state *state,
-			  git_hash_ctx *ctx, off_t *already_hashed_to,
+			  git_hash_ctx *ctx, git_hash_ctx *compat_ctx,
+			  off_t *already_hashed_to,
 			  int fd, size_t size, enum object_type type,
 			  const char *path, unsigned flags)
 {
@@ -105,6 +107,7 @@ static int stream_to_pack(struct bulk_checkin_state *state,
 	int status = Z_OK;
 	int write_object = (flags & HASH_WRITE_OBJECT);
 	off_t offset = 0;
+	const struct git_hash_algo *compat = the_repository->compat_hash_algo;
 
 	git_deflate_init(&s, pack_compression_level);
 
@@ -128,8 +131,11 @@ static int stream_to_pack(struct bulk_checkin_state *state,
 				size_t hsize = offset - *already_hashed_to;
 				if (rsize < hsize)
 					hsize = rsize;
-				if (hsize)
+				if (hsize) {
 					the_hash_algo->update_fn(ctx, ibuf, hsize);
+					if (compat)
+						compat->update_fn(compat_ctx, ibuf, hsize);
+				}
 				*already_hashed_to = offset;
 			}
 			s.next_in = ibuf;
@@ -194,11 +200,13 @@ static int deflate_to_pack(struct bulk_checkin_state *state,
 			   unsigned flags)
 {
 	off_t seekback, already_hashed_to;
-	git_hash_ctx ctx;
+	git_hash_ctx ctx, compat_ctx;
 	unsigned char obuf[16384];
 	unsigned header_len;
 	struct hashfile_checkpoint checkpoint = {0};
 	struct pack_idx_entry *idx = NULL;
+	const struct git_hash_algo *compat = the_repository->compat_hash_algo;
+	struct object_id compat_oid;
 
 	seekback = lseek(fd, 0, SEEK_CUR);
 	if (seekback == (off_t) -1)
@@ -208,6 +216,10 @@ static int deflate_to_pack(struct bulk_checkin_state *state,
 			       type_name(type), (uintmax_t)size) + 1;
 	the_hash_algo->init_fn(&ctx);
 	the_hash_algo->update_fn(&ctx, obuf, header_len);
+	if (compat) {
+		compat->init_fn(&compat_ctx);
+		compat->update_fn(&compat_ctx, obuf, header_len);
+	}
 
 	/* Note: idx is non-NULL when we are writing */
 	if ((flags & HASH_WRITE_OBJECT) != 0)
@@ -222,7 +234,8 @@ static int deflate_to_pack(struct bulk_checkin_state *state,
 			idx->offset = state->offset;
 			crc32_begin(state->f);
 		}
-		if (!stream_to_pack(state, &ctx, &already_hashed_to,
+		if (!stream_to_pack(state, &ctx, &compat_ctx,
+				    &already_hashed_to,
 				    fd, size, type, path, flags))
 			break;
 		/*
@@ -239,6 +252,8 @@ static int deflate_to_pack(struct bulk_checkin_state *state,
 			return error("cannot seek back");
 	}
 	the_hash_algo->final_oid_fn(result_oid, &ctx);
+	if (compat)
+		compat->final_oid_fn(&compat_oid, &compat_ctx);
 	if (!idx)
 		return 0;
 
@@ -253,6 +268,8 @@ static int deflate_to_pack(struct bulk_checkin_state *state,
 			   state->nr_written + 1,
 			   state->alloc_written);
 		state->written[state->nr_written++] = idx;
+		if (compat)
+			repo_add_loose_object_map(the_repository, result_oid, &compat_oid);
 	}
 	return 0;
 }

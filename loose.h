377b3c95284b0b826e1fb00405b0b510ae91408b
brian m. carlson <sandals@crustytoothpaste.net>
2021-04-04 23:44:30 +0000

    WIP: loose: allow writing data for submodules
    There are two types of lookup tables we need to write: one for loose
    objects and one for submodules.  Let's add a set of flags to control
    whether we write the data and where to write it and update the callers
    to call the function appropriately.
    
    WIP: bulk-checkin can probably be removed now that we have index-pack working
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- loose.h
+++ loose.h
@@ -9,13 +9,21 @@ struct loose_object_map {
 	kh_oid_map_t *to_storage;
 };
 
+/* Should we write this object to disk? */
+#define LOOSE_WRITE		(1 << 0)
+/* Write this object to the loose object index. */
+#define LOOSE_INDEX_LOOSE	(0 << 1)
+/* Write this object to the submodule object index. */
+#define LOOSE_INDEX_SUBMODULE	(1 << 1)
+#define LOOSE_INDEX_MASK	(1 << 1)
+
 void loose_object_map_init(struct loose_object_map **map);
 void loose_object_map_clear(struct loose_object_map **map);
 int repo_map_object(struct repository *repo, struct object_id *dest,
 		    const struct git_hash_algo *dest_algo, const struct object_id *src);
 int repo_add_loose_object_map(struct repository *repo, const struct object_id *oid,
-			      const struct object_id *compat_oid, int write);
+			      const struct object_id *compat_oid, int flags);
 int repo_read_loose_object_map(struct repository *repo);
-int repo_write_loose_object_map(struct repository *repo);
+int repo_write_loose_object_map(struct repository *repo, int flags);
 
 #endif

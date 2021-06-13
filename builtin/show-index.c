5664fed94f16bee50ece93e77750d5eddd7baea8
brian m. carlson <sandals@crustytoothpaste.net>
2021-06-13 19:11:30 +0000

    builtin/show-index: support v3 indices
    The show-index code currently supports only v1 and v2 indexes because it
    intentionally doesn't use the normal index code for reading indices.
    Teach it to support v3 indices.
    
    Refactor out the common code to process CRCs and offsets for v2 and v3
    indices into a function.  This causes us to need an additional loop to
    print out the results since in the v3 case, we may read the object IDs
    after this data, but this should not be a significant problem.
    
    Since we read from standard input, we cannot seek over the data we don't
    want, such as shortened object names, order tables, NUL padding bytes,
    and data for the algorithm we aren't interested in.  As a result,
    introduce a function to read and discard this data.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- builtin/show-index.c
+++ builtin/show-index.c
@@ -8,6 +8,56 @@ static const char *const show_index_usage[] = {
 	NULL
 };
 
+struct hash_format {
+	const struct git_hash_algo *algo;
+	uint32_t abbrev_len;
+	uint64_t offset;
+};
+
+struct entry {
+	struct object_id oid;
+	uint32_t crc;
+	uint64_t off;
+};
+
+static void discard_bytes(uint64_t to_discard)
+{
+	char discard[256];
+	while (to_discard) {
+		int chunk = to_discard > sizeof(discard) ? sizeof(discard) : to_discard;
+		if (fread(discard, chunk, 1, stdin) != 1)
+			die("unable to discard bytes");
+		to_discard -= chunk;
+	}
+}
+
+static void read_common_data(struct entry *entries, unsigned nr, unsigned *off64_nr)
+{
+	int i;
+	for (i = 0; i < nr; i++)
+		if (fread(&entries[i].crc, 4, 1, stdin) != 1)
+			die("unable to read crc %u/%u", i, nr);
+	for (i = 0; i < nr; i++)
+		if (fread(&entries[i].off, 4, 1, stdin) != 1)
+			die("unable to read 32b offset %u/%u", i, nr);
+	for (i = 0; i < nr; i++) {
+		uint64_t offset;
+		uint32_t off = ntohl(entries[i].off);
+		if (!(off & 0x80000000)) {
+			offset = off;
+		} else {
+			uint32_t off64[2];
+			if ((off & 0x7fffffff) != *off64_nr)
+				die("inconsistent 64b offset index");
+			if (fread(off64, 8, 1, stdin) != 1)
+				die("unable to read 64b offset %u", *off64_nr);
+			entries[i].off = (((uint64_t)ntohl(off64[0])) << 32) |
+						     ntohl(off64[1]);
+			(*off64_nr)++;
+		}
+	}
+}
+
 int cmd_show_index(int argc, const char **argv, const char *prefix)
 {
 	int i;
@@ -15,6 +65,9 @@ int cmd_show_index(int argc, const char **argv, const char *prefix)
 	unsigned int version;
 	static unsigned int top_index[256];
 	unsigned hashsz;
+	unsigned nformats;
+	unsigned hdrlen = 0;
+	struct hash_format *format, *cur_format = NULL;
 	const char *hash_name = NULL;
 	int hash_algo;
 	const struct option show_index_options[] = {
@@ -38,21 +91,51 @@ int cmd_show_index(int argc, const char **argv, const char *prefix)
 		die("unable to read header");
 	if (top_index[0] == htonl(PACK_IDX_SIGNATURE)) {
 		version = ntohl(top_index[1]);
-		if (version < 2 || version > 2)
+		if (version < 2 || version > 3)
 			die("unknown index version");
-		if (fread(top_index, 256 * 4, 1, stdin) != 1)
-			die("unable to read index");
+		if (version == 2) {
+			if (fread(top_index, 256 * 4, 1, stdin) != 1)
+				die("unable to read index");
+		} else if (version == 3) {
+			uint32_t hdr_len = 0;
+
+			if (fread(top_index, 3 * 4, 1, stdin) != 1)
+				die("unable to read header");
+			hdr_len = ntohl(top_index[0]);
+			nr = ntohl(top_index[1]);
+			nformats = ntohl(top_index[2]);
+			ALLOC_ARRAY(format, nformats);
+			for (int i = 0; i < nformats; i++) {
+				int algo;
+				if (fread(top_index, 4 * 4, 1, stdin) != 1)
+					die("unable to read header object format tables");
+				algo = hash_algo_by_id(ntohl(top_index[0]));
+				if (algo == GIT_HASH_UNKNOWN)
+					die("unknown hash algorithm id %08x", ntohl(top_index[0]));
+				format[i].algo = &hash_algos[algo];
+				format[i].abbrev_len = ntohl(top_index[1]);
+				format[i].offset = (((uint64_t)ntohl(top_index[2])) << 32) | ntohl(top_index[3]);
+				if (the_hash_algo == format[i].algo)
+					cur_format = &format[i];
+			}
+			hdrlen = 4 * 5 + 16 * nformats;
+			if (!cur_format)
+				die("specified hash algorithm not in this index");
+			discard_bytes(format[0].offset + (format[0].abbrev_len * nr) - hdrlen);
+		}
 	} else {
 		version = 1;
 		if (fread(&top_index[2], 254 * 4, 1, stdin) != 1)
 			die("unable to read index");
 	}
-	nr = 0;
-	for (i = 0; i < 256; i++) {
-		unsigned n = ntohl(top_index[i]);
-		if (n < nr)
-			die("corrupt index file");
-		nr = n;
+	if (version <= 2) {
+		nr = 0;
+		for (i = 0; i < 256; i++) {
+			unsigned n = ntohl(top_index[i]);
+			if (n < nr)
+				die("corrupt index file");
+			nr = n;
+		}
 	}
 	if (version == 1) {
 		for (i = 0; i < nr; i++) {
@@ -63,42 +146,50 @@ int cmd_show_index(int argc, const char **argv, const char *prefix)
 			offset = ntohl(entry[0]);
 			printf("%u %s\n", offset, hash_to_hex((void *)(entry+1)));
 		}
-	} else {
+	} else if (version == 2) {
+		struct entry *entries;
 		unsigned off64_nr = 0;
-		struct {
-			struct object_id oid;
-			uint32_t crc;
-			uint32_t off;
-		} *entries;
 		ALLOC_ARRAY(entries, nr);
 		for (i = 0; i < nr; i++) {
 			if (fread(entries[i].oid.hash, hashsz, 1, stdin) != 1)
 				die("unable to read sha1 %u/%u", i, nr);
 			entries[i].oid.algo = hash_algo_by_ptr(the_hash_algo);
 		}
-		for (i = 0; i < nr; i++)
-			if (fread(&entries[i].crc, 4, 1, stdin) != 1)
-				die("unable to read crc %u/%u", i, nr);
-		for (i = 0; i < nr; i++)
-			if (fread(&entries[i].off, 4, 1, stdin) != 1)
-				die("unable to read 32b offset %u/%u", i, nr);
+		read_common_data(entries, nr, &off64_nr);
 		for (i = 0; i < nr; i++) {
-			uint64_t offset;
-			uint32_t off = ntohl(entries[i].off);
-			if (!(off & 0x80000000)) {
-				offset = off;
-			} else {
-				uint32_t off64[2];
-				if ((off & 0x7fffffff) != off64_nr)
-					die("inconsistent 64b offset index");
-				if (fread(off64, 8, 1, stdin) != 1)
-					die("unable to read 64b offset %u", off64_nr);
-				offset = (((uint64_t)ntohl(off64[0])) << 32) |
-						     ntohl(off64[1]);
-				off64_nr++;
-			}
 			printf("%" PRIuMAX " %s (%08"PRIx32")\n",
-			       (uintmax_t) offset,
+			       (uintmax_t) entries[i].off,
+			       oid_to_hex(&entries[i].oid),
+			       ntohl(entries[i].crc));
+		}
+		free(entries);
+	} else {
+		struct entry *entries;
+		unsigned off64_nr = 0;
+		ALLOC_ARRAY(entries, nr);
+		if (cur_format == &format[0]) {
+			for (i = 0; i < nr; i++) {
+				if (fread(entries[i].oid.hash, format[0].algo->rawsz, 1, stdin) != 1)
+					die("unable to read %s %u/%u", format[0].algo->name, i, nr);
+				entries[i].oid.algo = hash_algo_by_ptr(format[0].algo);
+			}
+		} else {
+			discard_bytes((uint64_t)nr * format[0].algo->rawsz);
+		}
+		discard_bytes((uint64_t)nr * 4);
+		read_common_data(entries, nr, &off64_nr);
+		if (cur_format == &format[1]) {
+			discard_bytes(format[1].offset + (format[1].abbrev_len * nr) -
+				      ((4 + 4 + 4) * nr) - (8 * off64_nr) - format[0].offset);
+			for (i = 0; i < nr; i++) {
+				if (fread(entries[i].oid.hash, format[0].algo->rawsz, 1, stdin) != 1)
+					die("unable to read %s %u/%u", format[1].algo->name, i, nr);
+				entries[i].oid.algo = hash_algo_by_ptr(format[1].algo);
+			}
+		}
+		for (i = 0; i < nr; i++) {
+			printf("%" PRIuMAX " %s (%08"PRIx32")\n",
+			       (uintmax_t) entries[i].off,
 			       oid_to_hex(&entries[i].oid),
 			       ntohl(entries[i].crc));
 		}

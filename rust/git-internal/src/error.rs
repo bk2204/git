4863360509433d211882cdc37a2bdf8ba9ab1634
brian m. carlson <sandals@crustytoothpaste.net>
2025-04-21 22:27:39 +0000

    rust: add error types for parsing config
    In a future commit, we'll add support for parsing config files in Rust
    instead of in C and we'll want to be able to handle multiple different
    types of errors.  Add three new error types that we can use to handle
    various parsing problems.
    
    For the UnexpectedConfigData message, keep the error messages similar to
    those to those used in C, but provide a more descriptive reason as well.
    
    Signed-off-by: brian m. carlson <sandals@crustytoothpaste.net>

--- rust/git-internal/src/error.rs
+++ rust/git-internal/src/error.rs
@@ -2,9 +2,12 @@
 
 use std::borrow::Cow;
 use std::fmt::{self, Display};
+use std::path::PathBuf;
 use std::str::Utf8Error;
 
+use crate::config::ConfigOrigin;
 use crate::gettext::gettext as __;
+use crate::gettext::AsLocalizable;
 
 /// An error code usable from C.
 ///
@@ -14,6 +17,12 @@ use crate::gettext::gettext as __;
 pub enum ErrorCode {
     /// An invalid UTF-8 sequence was found.
     InvalidUtf8 = 1,
+    /// An error was found while parsing configuration.
+    ConfigIoError = 2,
+    /// Unexpected data was found when parsing configuration.
+    UnexpectedConfigData = 3,
+    /// The configuration value was not valid.
+    InvalidConfigValue = 4,
 }
 
 impl From<ErrorCode> for u32 {
@@ -31,6 +40,17 @@ pub enum Error {
     Annotated(Cow<'static, str>, Box<Error>),
     /// A string was required to be UTF-8, but was not.
     InvalidUtf8(std::str::Utf8Error),
+    /// An I/O error occurred when parsing configuration.
+    ConfigIoError(Option<PathBuf>, ConfigOrigin, u64, std::io::Error),
+    /// Unexpected data was found when parsing configuration.
+    ///
+    /// The string component should be a localized string stating what was expected or unexpected.
+    UnexpectedConfigData(Option<String>, ConfigOrigin, u64, String),
+    /// The configuration value was not valid.
+    ///
+    /// The first component is the key, the second is the value, and the last is a localized string
+    /// stating what was expected or unexpected.
+    InvalidConfigValue(Cow<'static, str>, Option<Cow<'static, str>>, String),
 }
 
 impl Display for Error {
@@ -43,6 +63,87 @@ impl Display for Error {
                 f,
                 "{}",
                 formatx!(__("invalid UTF-8 after byte {}"), e.valid_up_to()).unwrap()
+            ),
+            Self::ConfigIoError(Some(path), _, line, e) => write!(
+                f,
+                "{}",
+                formatx!(
+                    __("failed to parse {} (line {}) as config file: {}"),
+                    path.display(),
+                    line,
+                    e.as_localizable()
+                )
+                .unwrap(),
+            ),
+            Self::ConfigIoError(None, origin, line, e) => write!(
+                f,
+                "{}",
+                formatx!(
+                    __("failed to parse {} (line {}) as config file: {}"),
+                    origin,
+                    line,
+                    e.as_localizable()
+                )
+                .unwrap(),
+            ),
+            Self::UnexpectedConfigData(Some(name), ConfigOrigin::Blob, line, msg) => write!(
+                f,
+                "{}",
+                formatx!(__("bad config line {} in blob {}: {}"), line, name, msg).unwrap(),
+            ),
+            Self::UnexpectedConfigData(_, ConfigOrigin::Stdin, line, msg) => write!(
+                f,
+                "{}",
+                formatx!(__("bad config line {} in standard input: {}"), line, msg).unwrap(),
+            ),
+            Self::UnexpectedConfigData(Some(name), ConfigOrigin::SubmoduleBlob, line, msg) => {
+                write!(
+                    f,
+                    "{}",
+                    formatx!(
+                        __("bad config line {} in submodule blob {}: {}"),
+                        line,
+                        name,
+                        msg
+                    )
+                    .unwrap(),
+                )
+            }
+            Self::UnexpectedConfigData(Some(name), ConfigOrigin::File, line, msg) => write!(
+                f,
+                "{}",
+                formatx!(__("bad config line {} in file {}: {}"), line, name, msg).unwrap(),
+            ),
+            Self::UnexpectedConfigData(Some(name), ConfigOrigin::Cmdline, line, msg) => write!(
+                f,
+                "{}",
+                formatx!(
+                    __("bad config line {} in command line {}: {}"),
+                    line,
+                    name,
+                    msg
+                )
+                .unwrap(),
+            ),
+            Self::UnexpectedConfigData(Some(name), _, line, msg) => write!(
+                f,
+                "{}",
+                formatx!(__("bad config line {} in {}: {}"), line, name, msg).unwrap(),
+            ),
+            Self::UnexpectedConfigData(None, _, line, msg) => write!(
+                f,
+                "{}",
+                formatx!(__("bad config line {}: {}"), line, msg).unwrap(),
+            ),
+            Self::InvalidConfigValue(key, Some(value), msg) => write!(
+                f,
+                "{}",
+                formatx!(__("invalid config value for {} ({}): {}"), key, msg, value).unwrap(),
+            ),
+            Self::InvalidConfigValue(key, None, msg) => write!(
+                f,
+                "{}",
+                formatx!(__("invalid missing config value for {}: {}}"), key, msg).unwrap(),
             ),
         }
     }
@@ -58,6 +159,9 @@ impl Error {
     pub fn code(&self) -> ErrorCode {
         match self {
             Self::InvalidUtf8(..) => ErrorCode::InvalidUtf8,
+            Self::ConfigIoError(..) => ErrorCode::ConfigIoError,
+            Self::UnexpectedConfigData(..) => ErrorCode::UnexpectedConfigData,
+            Self::InvalidConfigValue(..) => ErrorCode::InvalidConfigValue,
             Self::Annotated(_, e) => e.code(),
         }
     }

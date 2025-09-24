31dc00b324d3578f4c8c41211223e908f76ec36b
brian m. carlson <bk2204@github.com>
2025-09-24 19:44:51 +0000

    t1017: add additional cross-algorithm testing
    We have testing for a SHA-256 server with SHA-1 compatibility and a
    SHA-1 client with SHA-256 compatibility.  Let's add tests for the other
    way around as well to make sure those work as well.
    
    Signed-off-by: brian m. carlson <bk2204@github.com>

--- t/t1017-interoperability.sh
+++ t/t1017-interoperability.sh
@@ -277,6 +277,7 @@ test_fetch_push sha256-to-sha256 sha256: sha256: --fsck
 test_fetch_push sha256-to-sha256-fancy sha256: sha256: --fsck --large-blob 512 --protocol 0
 test_fetch_push sha1-to-sha256-same sha1:sha256 sha1:sha256
 test_fetch_push sha256-to-sha1-same sha256:sha1 sha256:sha1
+test_fetch_push sha256-to-sha1-both sha256:sha1 sha1:sha256
 test_fetch_push sha1-to-sha256-main sha1: sha256:sha1
 test_fetch_push sha1-to-sha256-main-fancy sha1: sha256:sha1 --fsck --large-blob 512 --protocol 0
 test_fetch_push sha1-to-sha256-both sha1:sha256 sha256:sha1

dd3d15a2e29e9590e0399d41f983dce2eca6b702
brian m. carlson <bk2204@github.com>
2026-01-05 21:40:07 +0000

    upload-pack: emit object mappings for submodules
    In order to allow a clone or fetch with a compatibility algorithm in a
    repository with submodules, we must send a submodule mapping over the
    protocol because otherwise the client cannot correctly map the trees it
    receives.
    
    Add support for such a mapping by walking the history of the data we are
    sending, using arguments similar to those we pass to git pack-objects.
    For efficiency reasons, set up blob filtering so that we don't need to
    traverse blobs, only trees, since only trees can contain submodule
    references and the revision machinery does not provide any other support
    for traversing only trees.  Notably, trying to ask the revision
    machinery for trees and not blobs using options instead of filters
    causes it to die, so don't do that.
    
    While we do not yet support partial clones in compatibility mode,
    exclude promisor objects since they don't actually exist on disk and
    traversing them won't do anything useful.
    
    Signed-off-by: brian m. carlson <bk2204@github.com>

--- upload-pack.c
+++ upload-pack.c
@@ -18,6 +18,7 @@
 #include "commit.h"
 #include "diff.h"
 #include "revision.h"
+#include "list-objects.h"
 #include "list-objects-filter-options.h"
 #include "run-command.h"
 #include "connect.h"
@@ -26,6 +27,7 @@
 #include "string-list.h"
 #include "strvec.h"
 #include "trace2.h"
+#include "tree.h"
 #include "protocol.h"
 #include "upload-pack.h"
 #include "commit-graph.h"
@@ -331,8 +333,11 @@ static void compute_packing_args(struct upload_pack_data *pack_data,
 {
 	struct child_process *pack_objects = &pack_data->pack_objects;
 	struct strvec *stdin_objects = &pack_data->stdin_objects;
+	struct strvec *rev_args = &pack_data->submodule_revision_args;
 
 	child_process_init(pack_objects);
+
+	strvec_pushl(rev_args, "rev-list", "--objects", NULL);
 
 	if (!pack_data->pack_objects_hook)
 		pack_objects->git_cmd = 1;
@@ -361,8 +366,10 @@ static void compute_packing_args(struct upload_pack_data *pack_data,
 		strvec_push(&pack_objects->args, "--delta-base-offset");
 	if (pack_data->use_include_tag)
 		strvec_push(&pack_objects->args, "--include-tag");
-	if (repo_has_accepted_promisor_remote(the_repository))
+	if (repo_has_accepted_promisor_remote(the_repository)) {
 		strvec_push(&pack_objects->args, "--missing=allow-promisor");
+		strvec_push(rev_args, "--missing=allow-promisor");
+	}
 	if (pack_data->filter_options.choice) {
 		const char *spec =
 			expand_list_objects_filter_spec(&pack_data->filter_options);
@@ -988,6 +995,12 @@ static int send_one_shallow(const struct object_id *oid, void *data)
 	packet_writer_write(&d->data->writer, "%s %s",
 			    d->text, oid_to_hex(oid));
 	return 0;
+}
+
+static int send_one_mapped_submodule(const struct object_id *oid, void *unused UNUSED, void *data)
+{
+	/* The formatting is the same for both types of objects. */
+	return send_one_mapped_shallow(oid, data);
 }
 
 /* Returns 1 if a shallow list is sent or 0 otherwise */
@@ -1881,6 +1894,102 @@ static void compute_shallow_info(struct upload_pack_data *data)
 		deepen(data, INFINITE_DEPTH);
 }
 
+static int process_tree_entry_for_maps(const struct object_id *oid,
+				       struct strbuf *name UNUSED,
+				       const char *path UNUSED,
+				       unsigned int mode, void *cbdata)
+{
+	struct oidtree *entries = cbdata;
+
+	if (S_ISGITLINK(mode))
+		oidtree_insert(entries, oid);
+	return 0;
+}
+
+static void add_object_to_maps(struct object *obj, const char *name UNUSED,
+			       void *cbdata)
+{
+	struct tree *tree;
+	struct pathspec ps = { 0 };
+
+	if (obj->type != OBJ_TREE)
+		return;
+
+	tree = (struct tree *)obj;
+
+	ps.recursive = 1;
+	ps.has_wildcard = 1;
+	ps.max_depth = -1;
+
+	read_tree(the_repository, tree, &ps, process_tree_entry_for_maps,
+		  cbdata);
+}
+
+static void compute_submodule_maps(struct upload_pack_data *data,
+				   const struct git_hash_algo *hash_algo UNUSED,
+				   const struct git_hash_algo *map_hash_algo)
+{
+	struct rev_info revs;
+	int flags = 0;
+	struct setup_revision_opt s_r_opt = {
+		.allow_exclude_promisor_objects = 1,
+	};
+
+	if (!map_hash_algo)
+		return;
+
+	repo_init_revisions(the_repository, &revs, NULL);
+
+	/* We can't serve or map objects that are in a promisor pack, so make a
+	 * best-effort attempt to exclude them.
+	 */
+	revs.include_check = commit_is_not_in_promisor_pack;
+	revs.include_check_obj = object_is_not_in_promisor_pack;
+
+	list_objects_filter_copy(&revs.filter, &data->filter_options);
+	/*
+	 * We are only interested in trees when converting submodules, since
+	 * they're the only objects which hold submodules, so exclude blobs.
+	 */
+	parse_list_objects_filter(&revs.filter, "blob:none");
+
+	setup_revisions_from_strvec(&data->submodule_revision_args, &revs, &s_r_opt);
+
+	for (size_t i = 0; i < data->stdin_objects.nr; i++) {
+		const char *line = data->stdin_objects.v[i], *p;
+		int len = strlen(line);
+		if (!len)
+			break;
+		if (*line == '-') {
+			if (!strcmp(line, "--not")) {
+				flags ^= UNINTERESTING;
+				continue;
+			}
+			if (skip_prefix(line, "--shallow ", &p)) {
+				struct object_id oid;
+				if (get_oid_hex(p, &oid))
+					die("not an object name '%s'", p);
+				/*
+				 * We've already registered this shallow, so we
+				 * don't need to do anything else.
+				 */
+				continue;
+			}
+			die(_("not a rev '%s'"), line);
+		}
+		if (handle_revision_arg(line, &revs, flags, REVARG_CANNOT_BE_FILENAME))
+			die(_("bad revision '%s'"), line);
+	}
+
+	if (prepare_revision_walk(&revs))
+		die(_("revision walk setup failed"));
+	traverse_commit_list(&revs,
+			     NULL, add_object_to_maps,
+			     &data->submodule_references);
+
+	release_revisions(&revs);
+}
+
 static void send_object_map_info(struct upload_pack_data *data,
 				 const struct git_hash_algo *map_algo,
 				 bool headerfooter)
@@ -1898,7 +2007,8 @@ static void send_object_map_info(struct upload_pack_data *data,
 	if (!map_algo ||
 	    (strmap_empty(&data->wanted_refs) &&
 	     !data->shallow_response.nr &&
-	     !data->unshallow_response.nr))
+	     !data->unshallow_response.nr &&
+	     oidtree_is_empty(&data->submodule_references)))
 		return;
 
 	if (headerfooter)
@@ -1926,6 +2036,11 @@ static void send_object_map_info(struct upload_pack_data *data,
 
 		oidset_insert(&seen_wanted_refs, e->value);
 	}
+
+	d.text = "submodule";
+	oidtree_each(&data->submodule_references,
+		     null_oid(the_repository->hash_algo),
+		     0, send_one_mapped_submodule, &d);
 
 	if (headerfooter)
 		packet_writer_delim(&data->writer);
@@ -2023,6 +2138,8 @@ int upload_pack_v2(struct repository *r, struct packet_reader *request)
 					     data.uri_protocols.nr ?
 					     &data.uri_protocols : NULL,
 					     request->hash_algo);
+			compute_submodule_maps(&data, request->hash_algo,
+					       request->map_hash_algo);
 			send_object_map_info(&data, request->map_hash_algo, true);
 			send_wanted_ref_info(&data);
 			send_shallow_info(&data);
